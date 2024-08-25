8b7f01ecb3cd77427d2de2e99d35ee230300027e
asahaida <156024925+asahaida@users.noreply.github.com>
2024-08-24 22:01:07 -0500

    updaitng

---
+++ template.js
@@ -0,0 +1,33 @@
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// function myMap() {
+//   myCenter=new google.maps.LatLng(41.878114, -87.629798);
+//   var mapOptions= {
+//     center:myCenter,
+//     zoom:12, scrollwheel: false, draggable: false,
+//     mapTypeId:google.maps.MapTypeId.ROADMAP
+//   };
+//   var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+//
+//   var marker = new google.maps.Marker({
+//     position: myCenter,
+//   });
+//   marker.setMap(map);
+// }
