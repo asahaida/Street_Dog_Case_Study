dd49b69002e2feec7b168bcd454de5a3ec9238e0
asahaida <156024925+asahaida@users.noreply.github.com>
2024-08-24 21:52:31 -0500

    working on nav bar

---
+++ index.js
@@ -0,0 +1,29 @@
+
+$(".searchButton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")) {
+      $(".searchBar").css("height", "100vh");
+      $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    }
+    else {
+      $(".searchBar").css('height', "0vh");
+      $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+    }
+  });
+  
+  $(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+  });
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+  } else {
+      x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
